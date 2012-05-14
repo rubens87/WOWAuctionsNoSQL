8a2ab6b009853247cd7cc3331c7bf281cf1ca2f1
Lukasz Kedron <wheresmyjaw@gmail.com>
2012-05-14 09:27:01 +0200

    salem change

--- salem_auctions.py
+++ salem_auctions.py
@@ -9,37 +9,55 @@ Feel free to take it, modify, expand, explore!
 
 What it does?
 It takes json file  representing server auctions and puts it into mongoDB
-Its part of NoSQL course homework for dr W.Bzyl (that tutorials guy from wykop - polish reddit/digg)
-Created by the Internet, borrowed and coded by Lukasz Kedron (rubens87@github)
+
+What has to be changed?Many things!
+First of all we want to make it more flexible (-args) *updated DONE
+Secondly maybe we change a bit that json to represent data in diffrend way.
+
+
+It is part of NoSQL course homework for dr W.Bzyl (that tutorials guy from wykop - polish reddit/digg)
+
+Created by the Internet, 
+borrowed and coded by Lukasz Kedron (rubens87@github)
 """
 
 import pymongo
 import json
 from pymongo import Connection
+from optparse import OptionParser
 
 """
 here is going to be config the whole thing:
 """
 
+parser = OptionParser()
+parser.add_option("-f", "--file", dest="jsonname", help="give json filename, as a argument", default="auctionsBL.json", metavar="FILE")
+parser.add_option("-n", "--name", dest="dbname", help="give name of database to create", default="burning_legion", metavar="DBNAME")
+parser.add_option("-m", "--host", dest="dbhost", help="give host name of database", default="localhost", metavar="DBHOST")
+parser.add_option("-p", "--port", dest="dbport", help="give port", default=27017, metavar="DBPORT")
+(options, args) = parser.parse_args()
+
+
+
 #dbsetup
 
-port = 27017 #now
-host = 'localhost'
+port = options.dbport
+host = options.dbhost
 
 #dbchooser
-dbname = "burning_legion"
+dbname = options.dbname
 
 #filechooser
-json_sourceFile = "auctionsBL.json"
+json_sourceFile = options.jsonname
 
 
 
 
 #Connecting to db
-connection = Connection('localhost', 27017)
-f=open("auctionsBL.json", "r")
+connection = Connection(host, options.dbport)
+f=open(json_sourceFile, "r")
 post = json.loads(f.read())
-db = connection['wow-auctions']
+db = connection[dbname]
 posts = db.auctions
 
 #adding alliance
